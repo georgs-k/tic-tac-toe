48b27b5adf2c68139b52c4468bbab301fcb842a9
georgijs.kadolciks <georgijs.kadolciks@accenture.com>
2023-01-13 10:03:10 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+	java
+	id("org.springframework.boot") version "2.7.7"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+}
+
+group = "tinygames"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/java/tinygames/tictactoe/TicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package tinygames.tictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
