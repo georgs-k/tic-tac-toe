3a8fd7e68028226281c4152366c758acb368c5a8
georgijs.kadolciks <georgs.k@apollo.lv>
2024-09-25 09:59:51 +0300

    Add JaCoCo plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "2.7.7"
 	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    id("jacoco")
     id("org.sonarqube") version "5.0.0.4638"
 }
 
@@ -33,4 +34,12 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
 }
